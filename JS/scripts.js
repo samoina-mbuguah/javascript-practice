e5e2746b31d10eb5029a04866c78f5629b187e57
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-10 20:36:45 +0300

    Add code to grade a testscore submitted by the user

--- JS/scripts.js
+++ JS/scripts.js
@@ -13,9 +13,24 @@ if (BMI<18.5) {
 }
 
 // coffee and tea cafe
-var answer=confirm("Would you like some tea?");
-if (answer="Yes") {
-  console.log("Your hot cup of tea will e served shortly");
+alert("Welcome to the Godzilla cafe :)");
+var answer=prompt("Would you like some tea?");
+if (answer==="Yes") {
+  console.log("Your hot cup of tea will be served shortly");
 } else {
   console.log("Your cuppa coffee will be served shortly");
 }
+
+//Display your test score
+var testScore=parseInt(prompt("Please enter your test score: "))
+if (testScore>=0 && testScore<=20) {
+  console.log("Your test score grade is E");
+} else if (testScore>=21 && testScore<=40) {
+    console.log("Your test score grade is D");
+  } else if (testScore>=41 && testScore<=60) {
+    console.log("Your test score grade is C");
+  } else if (testScore>=61 && testScore<=80) {
+    console.log("Your test score grade is B");
+  } else if (testScore>=81 && testScore<=100) {
+    console.log("Your test score grade is D");
+  }
