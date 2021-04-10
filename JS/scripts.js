edbc519e97ec72f89d8f4fb0ab3d82de3767a0de
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-10 17:44:18 +0300

    Add bootstrap to the JS folder

---
+++ JS/scripts.js
@@ -0,0 +1,13 @@
+var height=prompt("Please enter your height in meters: ");
+var weight=prompt("Please enter your weight in kilograms:");
+newHeight=parseInt(height * height);
+newWeight=parseInt(weight);
+
+let BMI=(newWeight/newHeight);
+if (BMI<18.5) {
+  console.log("Your BMI indicates that you may be underweight.");
+} else if (BMI>=18.5 && BMI<=24.9) {
+  console.log("Your BMI is within the healthy range");
+} else {
+  console.log("Your BMI indicates that you may be overweight");
+}
---
+++ operators.js
@@ -0,0 +1,14 @@
+ let x=5;
+ let y=10;
+
+ let result=x*y;
+ console.log(result)
+
+/***** Assignement Operators *****/
+
+let myDigit=10;
+
+myDigit =+5;       //myDigit=myDigit+5//
+console.log(myDigit);
+
+
